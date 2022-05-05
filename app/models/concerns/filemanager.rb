945311536da443192c4d41f9a74f6a88e9b9bebf
itsmattpaw <itsmattpaw@gmail.com>
2022-05-05 07:16:30 -0400

    wrote slvc data update method, basic function

--- app/models/concerns/filemanager.rb
+++ app/models/concerns/filemanager.rb
@@ -1,3 +1,5 @@
 class FileManager
-    #use 
+    #use axlsx gem to create and format excel files
+    #use csv gem to read and create csv files
+    
 end
--- app/models/concerns/slvcgenerator.rb
+++ app/models/concerns/slvcgenerator.rb
@@ -3,4 +3,25 @@ class SlvcGenerator
         #page one is a short table with count of stores organized by location
         #page two is the store list with ALL SLvC data appended to the end
         #should export as a formatted excel file
+    def self.slvc_data_update(file)
+        h = CSV.read(file)
+        h.each do |row|
+            SlvcData.create(
+                zip: row[0]
+                company: row[1]
+                market: row[2]
+                wrap: row[3]
+                hh_count: row[4]
+                newspaper: row[5]
+                highest_circulation: row[6]
+                newspaper_penetration: row[7]
+                county: row[8]
+                county_code: row[9]
+                dma_num: row[10]
+                dma_name: row[11]
+                frequency: row[12]
+                ihd:row[13]
+            )
+        end
+    end
 end
